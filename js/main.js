1717c7873c53db28d4df20eaf1772d3e933cd337
nadal7x <nadal7x@hotmail.com>
2021-12-07 13:51:23 +0100

    try again

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import {ckeditor} from '../node_modules/@ckeditor/ckeditor5-build-classic/build/ckeditor.js'
+import {ckeditor5} from '../node_modules/@ckeditor/ckeditor5-build-classic/build/ckeditor.js'
 import {submitForm} from './form.js';
 import {tabs} from './tabs.js';
 
@@ -6,4 +6,4 @@ import {tabs} from './tabs.js';
 
 submitForm();
 tabs();
-ckeditor();
+ckeditor5();
