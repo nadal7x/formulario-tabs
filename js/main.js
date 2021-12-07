faea060920953ba19d14752e95c2439a91e39b47
nadal7x <nadal7x@hotmail.com>
2021-12-07 13:29:51 +0100

    fingerprint works?

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-import {} from 'node_modules/@ckeditor/ckeditor5-build-classic/build/ckeditor.js'
+import {ckeditor} from 'node_modules/@ckeditor/ckeditor5-build-classic/build/ckeditor.js'
 import {submitForm} from './form.js';
 import {tabs} from './tabs.js';
 
 submitForm();
 tabs();
+ckeditor();
