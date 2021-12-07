e3f0005765cbad13cece15c89fae1b76e9839870
nadal7x <nadal7x@hotmail.com>
2021-12-07 15:26:07 +0100

    link correcto editor

--- js/editor.js
+++ js/editor.js
@@ -1,11 +1,12 @@
 import ClassicEditor from '../node_modules/@ckeditor/ckeditor5-build-classic';
 
-
-ClassicEditor
+export const editor = () => {
+ ClassicEditor
 	.create( document.querySelector( '#editor' ) )
 	.then( editor => {
 		window.editor = editor;
 	} )
 	.catch( error => {
 		console.error( 'There was a problem initializing the editor.', error );
-	} );
+    } );
+}
