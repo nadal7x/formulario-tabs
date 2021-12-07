ebaa3be17b5f1c2efc86b26c882a97ae313e1823
nadal7x <nadal7x@hotmail.com>
2021-12-07 15:03:46 +0100

    funciona?

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import {ckeditor} from '/node_modules/@ckeditor/ckeditor5-build-classic/build/ckeditor.js'
+//import {ckeditor} from '/node_modules/@ckeditor/ckeditor5-build-classic/build/ckeditor.js'
 import {submitForm} from './form.js';
 import {tabs} from './tabs.js';
 
