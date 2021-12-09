2708c31b2c47079f92fc460e9b015369787a1ac1
nadal7x <nadal7x@hotmail.com>
2021-12-09 09:41:26 +0100

    data multipe text

--- js/editor.js
+++ js/editor.js
@@ -1,33 +1,34 @@
-export const editorArea = () => {
+export const editor = () => {
     let editors = document.querySelectorAll( '.editor' );
-    window.editor = [];
-    editors.forEach(element =>   
+    window.editor =[];
+    editors.forEach( function(element, index) {
         ClassicEditor
-            .create( element, {
-                toolbar: {
-                    items: [
-                        'bold',
-                        'italic',
-                        'link',
-                        'bulletedList',
-                        'numberedList',
-                        '|',
-                        'outdent',
-                        'indent',
-                        '|',
-                        'blockQuote',
-                        'undo',
-                        'redo'
-                    ]
-                },
-                language: 'es',
-                licenseKey: '',
-            } )
-            .then( newEditor => {
-                editor.push = newEditor;
-            } )
-            .catch( error => {
-                console.error( 'There was a problem initializing the editor.', error );
-            } )
-    );
+        .create( element, {
+            toolbar: {
+                items: [
+                    'bold',
+                    'italic',
+                    'link',
+                    'bulletedList',
+                    'numberedList',
+                    '|',
+                    'outdent',
+                    'indent',
+                    '|',
+                    'blockQuote',
+                    'undo',
+                    'redo'
+                ]
+            },
+            language: 'es',
+            licenseKey: '',
+        } )
+        .then( editor => {
+            editor.push = element[index];
+        } )
+        .catch( error => {
+            console.error( 'There was a problem initializing the editor.', error );
+        } ); 
+    });
+ 
 }
