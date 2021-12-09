766d49f58c833c4a20354906761266d7ecd762d8
nadal7x <nadal7x@hotmail.com>
2021-12-09 09:58:13 +0100

    data multipe text

--- js/editor.js
+++ js/editor.js
@@ -1,34 +1,33 @@
 export const editor = () => {
-    let editors = document.querySelectorAll( '.editor' );
-    window.editor =[];
+    let editors = document.querySelector( '.editors' );
+    window.editor = [];
     editors.forEach( function(element, index) {
         ClassicEditor
-        .create( element, {
-            toolbar: {
-                items: [
-                    'bold',
-                    'italic',
-                    'link',
-                    'bulletedList',
-                    'numberedList',
-                    '|',
-                    'outdent',
-                    'indent',
-                    '|',
-                    'blockQuote',
-                    'undo',
-                    'redo'
-                ]
-            },
-            language: 'es',
-            licenseKey: '',
-        } )
-        .then( editor => {
-            editor.push = element[index];
-        } )
-        .catch( error => {
-            console.error( 'There was a problem initializing the editor.', error );
-        } ); 
+            .create( document.querySelector( '#editor' + index ), {
+                toolbar: {
+                    items: [
+                        'bold',
+                        'italic',
+                        'link',
+                        'bulletedList',
+                        'numberedList',
+                        '|',
+                        'outdent',
+                        'indent',
+                        '|',
+                        'blockQuote',
+                        'undo',
+                        'redo'
+                    ]
+                },
+                language: 'es',
+                licenseKey: '',
+            } )
+            .then( newEditor => {
+                window.editor.push(newEditor);
+            } )
+            .catch( error => {
+                console.error( 'There was a problem initializing the editor.', error );
+            } );
     });
- 
 }
